9efe46eae3868e6e509453527d26b4eef465251a
gwyong <gwyong@umich.edu>
2022-03-29 18:01:07 +0900

    update: update xer2csv.py file.

--- neom/xer2csv.py
+++ neom/xer2csv.py
@@ -1,18 +1,62 @@
 ## This code is a modified version of https://github.com/constantintcacenco/xer-to-csv-converter.
-try:
-    import xer2csv
-    from xer2csv import XerToCsvConverter
-except:
-    from pip._internal import main as pip
-    pip(['install', '--user', 'xer2csv'])
-    import xer2csv
-    from xer2csv import XerToCsvConverter
+import os
+import pandas as pd
 
-import sys, os
+class XerToCsvConverter:
+    def __init__(self, lang="en"):
+        self.tables = []
+        self.lang = lang.lower()
+        if self.lang == "ko":
+            self.encoding = "cp949"
+        else:
+            self.encoding = "utf8"
 
-converter   = XerToCsvConverter()
-xerFilePath = sys.argv[1]
-csvFilePath = sys.argv[2]
+    def readXer(self, filePath):
+        with open(filePath, encoding=self.encoding, errors="ignore") as f:
+            content = f.read()
+            tables  = content.split("%T")
+            self.tables = tables[1:]
 
-converter.read_xer(xerFilePath)
-converter.convert_to_csv(csvFilePath)
+    ## auxiliary function (no call from user needed)
+    def checkOutputDir(self, outputLocation):
+        if not os.path.exists(outputLocation):
+            os.makedirs(outputLocation)
+
+    ## auxiliary function (no call from user needed)
+    def checkMissingValues(self, columns, rowsList):
+        for row in rowsList:
+            if len(columns) > len(row):
+                row[len(columns):len(row)] = [None]*(len(columns) - len(row))
+        return rowsList
+
+    def convert2csv(self, outputPath):
+        for table in self.tables:
+            tableName = table.split()[0]
+            fields     = table.split(r"%F")[1].split("\n")[0].split()
+            
+            rows = table.split("%R")[1:]
+            rowsList = [r.strip().split("\t") for r in rows]
+            checkedRowsList = self.checkMissingValues(fields, rowsList)
+
+            df = pd.DataFrame(checkedRowsList, columns=fields, index=None)
+            self.checkOutputDir(outputPath)
+            csvFilePath = os.path.join(outputPath, tableName + ".csv")
+            if self.lang == "ko":
+                df.to_csv(csvFilePath, encoding="utf-8-sig")
+            else:
+                df.to_csv(csvFilePath, encoding=self.encoding)
+
+pathXER   = r"./inputs/file.xer" # target XER file directory
+pathCSVs  = r"./outputs/" # target folder directory to extract several CSV files.
+
+lang = "ko" # select language between "en" and "ko".
+
+converter = XerToCsvConverter(lang=lang) # default lang is "en".
+converter.readXer(pathXER)
+
+outputSubDir = os.path.join(pathCSVs, pathXER.split(".xer")[0].split("/")[-1])
+
+if not os.path.exists(outputSubDir):
+    os.makedirs(outputSubDir)
+
+converter.convert2csv(outputSubDir)
